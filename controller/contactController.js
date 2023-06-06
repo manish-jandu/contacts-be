4dc2fd82a3fabe48568bc2ce3173eba9735f9a6a
manish-jandu <manishjandu1@gmail.com>
2023-06-06 21:48:13 +0530

    controller added + routes setup

---
+++ controller/contactController.js
@@ -0,0 +1,45 @@
+//@desc Get all Contacts
+//@route GET /api/contacts
+//@access public
+const getContacts = (req,res)=>{
+    res.status(200).json({message:"Get all Contacts"});
+};
+
+//@desc Create New Contact
+//@route POST /api/contacts
+//@access public
+const createContact = (req,res)=>{
+    res.status(201).json({message:"Create Contact"});
+};
+
+
+//@desc GET a single Contact
+//@route GET /api/contacts/id
+//@access public
+const getContact = (req,res)=>{
+    res.status(200).json({message:`Get Contact for ${req.params.id}`});
+}
+
+//@desc Update Contact
+//@route PUT /api/contacts/id
+//@access public
+const updateContact = (req,res)=>{
+    res.status(200).json({message:`Update Contact for ${req.params.id}`});
+};
+
+
+//@desc Delete Contact
+//@route DELETE /api/contacts/id
+//@access public
+const deleteContact = (req,res)=>{
+    res.status(200).json({message:`Contact deleted for ${req.params.id}`});
+};
+
+
+module.exports = { 
+    getContacts,
+    createContact,
+    getContact,
+    updateContact,
+    deleteContact 
+};
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,26 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const { 
+    getContacts,
+    createContact,
+    getContact,
+    updateContact,
+    deleteContact 
+} = require('../controller/contactController');
 
-router.route('/').get((req,res)=>{
-    res.status(200).json({message:"Get all Contacts"});
-});
 
-router.route('/').post((req,res)=>{
-    res.status(200).json({message:"Create Contact"});
-});
+router.route('/').get(getContacts).post(createContact);
 
-router.route('/:id').get((req,res)=>{
-    res.status(200).json({message:`Get Contact for ${req.params.id}`});
-});
-
-router.route('/:id').put((req,res)=>{
-    res.status(200).json({message:`Update Contact for ${req.params.id}`});
-});
-
-router.route('/:id').delete((req,res)=>{
-    res.status(200).json({message:`Contact deleted for ${req.params.id}`});
-});
-
+router.route('/:id').get(getContact).put(updateContact).delete(deleteContact);
 
 module.exports = router;
