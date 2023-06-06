b4f6e4cfb5bc10f7156faf0953f21d1764848410
manish-jandu <manishjandu1@gmail.com>
2023-06-06 10:18:42 +0530

    added dot env file

--- Server.js
+++ Server.js
@@ -1,1 +1,10 @@
-console.log("server running");
+const express = require('express');
+const dotEnv = require('dotenv').config();
+const app = express();
+
+
+const PORT = process.env.PORT;
+
+app.listen(PORT,()=>{
+    console.log(`server started on ${PORT}...`);
+});
