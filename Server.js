c344de5757227de59b0ba52430570e20f5c714cd
manish-jandu <manishjandu1@gmail.com>
2023-06-07 17:26:58 +0530

    mongoose setp

--- Server.js
+++ Server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const dotEnv = require('dotenv').config();
 const app = express();
 const errorHanlder = require('./middleware/errorHanlder');
+const connectDb = require('./config/dbConnection');
 
 const PORT = process.env.PORT;
+
+connectDb();
 
 app.use(express.json());
 app.use('/api/contacts',require('./routes/contactRoutes'));
