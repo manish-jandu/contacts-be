6561387648b5905ee86e4a09fdb42a49a26c95b4
manish-jandu <manishjandu1@gmail.com>
2023-06-07 22:42:35 +0530

    register user setup

--- controller/userController.js
+++ controller/userController.js
@@ -1,11 +1,38 @@
 const ansyncHandler = require("express-async-handler");
+const bcyrpt = require("bcrypt");
+const User = require("../models/UserModel");
 
 //@desc Register New User
 //@route POST /api/register
 //@access public
 const registerUser = ansyncHandler(
     async (req,res)=>{
-        res.status(200).json({message:"Register the user"});
+        const {username,email,password} = req.body;
+        if(!username || !email || !password){
+            res.status(400);
+            throw new Error("All Fields are mandatory");
+        }
+
+        const userAvailable = await User.findOne({email});
+        if(userAvailable){
+            res.status(400);
+            throw new Error("User already Exists");
+        }
+
+        //Hash Password
+        const hashedPass = await bcyrpt.hash(password,10);
+        const user = await User.create({
+            username,
+            email,
+            password: hashedPass
+        });
+
+        if(user){
+            res.status(201).json({  _id:user._id, email:user.email});
+        } else {
+            res.status(400);
+            throw new Error("User data is not valid");
+        }    
     }
 );
 
